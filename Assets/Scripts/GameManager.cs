2180da168e65fbb1ebddd057178ff6b1b2cea20e
HjortheimerDaniel <147121561+HjortheimerDaniel@users.noreply.github.com>
2024-05-16 17:03:26 +0900

    Started working on the UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,13 +18,13 @@ public class GameManager : MonoBehaviour
         //string debugText = "";
 
 
-        map = new int[,]
+        map = new int[,] //1 = player, 2 = block, 3= goal
         {
 
-        { 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 3 },
         { 0, 0, 1, 2, 0, 0 },
         { 0, 0, 0, 2, 0, 0 },
-        { 0, 0, 0, 0, 0, 0 },
+        { 0, 3, 0, 0, 0, 0 },
 
         };
         field = new GameObject //create a gameobject
@@ -46,11 +46,13 @@ public class GameManager : MonoBehaviour
                 if (map[y, x] == 1) //found player
                 {
                     field[y, x] = Instantiate(playerPrefab, new Vector3(x, map.GetLength(0) - y, 0), Quaternion.identity);
+                    
                 }
 
                 if (map[y, x] == 2) //found block
                 {
                     field[y, x] = Instantiate(boxPrefab, new Vector3(x, map.GetLength(0) - y, 0), Quaternion.identity);
+                    break;
                 }
 
                 //debugText += map[y, x].ToString() + ",";
@@ -79,7 +81,7 @@ public class GameManager : MonoBehaviour
         {
             var playerIndex = GetPlayerIndex();
 
-            MoveNumber(playerIndex, playerIndex + new Vector2Int(-1,0));
+            MoveNumber(playerIndex, playerIndex + new Vector2Int(-1, 0));
 
             //PrintArray();
         }
@@ -100,6 +102,11 @@ public class GameManager : MonoBehaviour
             MoveNumber(playerIndex, playerIndex + new Vector2Int(0, -1));
 
             //PrintArray();
+        }
+
+        if (IsCleared())
+        {
+            Debug.Log("Clear");
         }
     }
 
@@ -137,7 +144,7 @@ public class GameManager : MonoBehaviour
 
     bool MoveNumber(Vector2Int moveFrom, Vector2Int moveTo)
     {
-    
+
         if (moveTo.y < 0 || moveTo.y >= field.GetLength(0)) //if we are outside of the map
         {
             return false; //dont return anything
@@ -160,20 +167,20 @@ public class GameManager : MonoBehaviour
             }
         }
 
-            // if (map[moveTo] == 2) //if there is a 2 where we are trying to go
-            // {
-            //     int velocity = moveTo - moveFrom;
-            //     bool success = MoveNumber(2, moveTo, moveTo + velocity);
-            //     if (!success)
-            //     {
-            //         return false;
-            //     }
-            // }
-            //
-            // map[moveTo] = number; //the place we moved to becomes 1
-            // map[moveFrom] = 0; //the place we came from becomes 0
-            //
-            // return true;
+        // if (map[moveTo] == 2) //if there is a 2 where we are trying to go
+        // {
+        //     int velocity = moveTo - moveFrom;
+        //     bool success = MoveNumber(2, moveTo, moveTo + velocity);
+        //     if (!success)
+        //     {
+        //         return false;
+        //     }
+        // }
+        //
+        // map[moveTo] = number; //the place we moved to becomes 1
+        // map[moveFrom] = 0; //the place we came from becomes 0
+        //
+        // return true;
 
         field[moveTo.y, moveTo.x] = field[moveFrom.y, moveFrom.x];
         field[moveFrom.y, moveFrom.x].transform.position = new Vector3(moveTo.x, map.GetLength(0) - moveTo.y, 0);
@@ -181,14 +188,41 @@ public class GameManager : MonoBehaviour
         return true;
 
 
-    }}
+    }
+
+    bool IsCleared()
+    {
+        List<Vector2Int> goals = new List<Vector2Int>();
+
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                if (map[y, x] == 3) //found a goal
+                {
+                    goals.Add(new Vector2Int(x, y)); //add the pos to goals
+                }
+
+            }
+        }
+
+        for (int i = 0; i < goals.Count; i++)
+        {
+            GameObject f = field[goals[i].y, goals[i].x];
+            if (f == null || f.tag != "Box") //if were not on a mapchip point where there is a "3", or if whats on top of it isnt a Box
+            {
+                return false; //this is false
+            }
+        }
+
+        return true;
+
+    }
+
+}
 
 
-    //if (playerIndex < map.Length - 1) //if were not at the end
-    //{
-    //    map[playerIndex + 1] = 1; //move to the right
-    //    map[playerIndex] = 0; //the place we were at becomes zero
-    //}
+    
 
 
 
